aa7eb1438c596c4f7196a2a74d4770636fc8c251
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-10-11 12:11:39 -0400

    added shop catalog

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -8,16 +8,9 @@ const Shop = ({ username, urls }) => {
       <h1>Welcome {username}</h1>
       <p>Catalog</p>
       <StyledCatalog>
-        <Item bgUrl={urls[0]} key={0} />
-        <Item bgUrl={urls[1]} key={1} />
-        <Item bgUrl={urls[2]} key={2} />
-        <Item bgUrl={urls[3]} key={3} />
-        <Item bgUrl={urls[4]} key={4} />
-        <Item bgUrl={urls[5]} key={5} />
-        <Item bgUrl={urls[6]} key={6} />
-        <Item bgUrl={urls[7]} key={7} />
-        <Item bgUrl={urls[8]} key={8} />
-        <Item bgUrl={urls[9]} key={9} />
+        {urls.map((imgStr, i) => (
+          <Item bgUrl={imgStr} key={i} />
+        ))}
       </StyledCatalog>
     </StyledShop>
   );
